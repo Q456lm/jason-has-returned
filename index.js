7f5505cb0c40ee49b3ce2df8931b163e8cbcc9b3
Q456lm <cbbunting1@my.waketech.edu>
2025-04-24 13:14:22 -0400

    So the creator pronounces in Jason, A bunch of dumbasses I sit next to say ja-son, I think Jason, Mr. Gardner says Jason, All my actual friends say Jason, my Mom says Jason, My Dad says Jason, My Dog says Jason, Karim says Jason, My Next Door Neighboor says Jason, Maddy says Jason, George Lucas says Jason, I wonder how I shoul pronouce it?

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const n1 = document.getElementById("name1");
+const n2 = document.getElementById("name2");
+const cor = document.getElementById("course");
+const sec = document.getElementById("sec_num");
+const rol = document.getElementById("role");
+const sub = document.getElementById("submit");
+sub.addEventListener('click',logit);
 
+function logit(){
+    let jason = {
+        "firstName":n1.value,
+        "lastName":n2.value,
+        "course":cor.value,
+        "section":sec.value,
+        "role": rol.value,
+    }
+    console.log(jason);
+    console.log(JSON.stringify(jason));
+    event.preventDefault();
+}
